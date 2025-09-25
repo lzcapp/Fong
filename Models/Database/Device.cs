c97a278bb52f4d1065fabfaa9af4dfa60bd5b1f0
Laurence Luo <admin@lzc.app>
2025-09-25 17:18:01 +0800

    Add column mappings to Device entity
    Added [Column] attributes to Device class properties to explicitly map them to database column names. This improves clarity and ensures correct mapping between the entity and the database schema.

--- Models/Database/Device.cs
+++ Models/Database/Device.cs
@@ -1,30 +1,40 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Fong.Models.Database {
     public class Device {
-        [Key] // Primary key
+        [Key][Column("id")] // Primary key
         public long Id { get; set; }
-
+        
+        [Column("mac")]
         public string Mac { get; set; } = string.Empty;
         
-        [Required]
+        [Required][Column("ip")]
         public string? Ip { get; set; }
 
-        [Required]
+        [Required][Column("state")]
         public int State { get; set; } = -1;
         
+        [Column("name")]
         public string Name { get; set; } = string.Empty;
         
+        [Column("type")]
         public string Type { get; set; } = string.Empty;
         
+        [Column("vendor")]
         public string Vendor { get; set; } = string.Empty;
         
+        [Column("model")]
         public string Model { get; set; } = string.Empty;
         
+        [Column("contact_id")]
         public string ContactId { get; set; } = string.Empty;
         
+        [Column("first_seen")]
         public long? FirstSeen { get; set; }
         
+        
+        [Column("last_changed")]
         public long? LastChanged { get; set; }
     }
 }
