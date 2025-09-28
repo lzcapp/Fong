6d1c2cc04d06ef6425cb19f615bbd584b1028a0d
Laurence Luo <admin@lzc.app>
2025-09-28 15:01:18 +0800

    Update Devices.cs

--- Controllers/Devices.cs
+++ Controllers/Devices.cs
@@ -17,16 +17,17 @@ namespace Fong.Controllers {
         [HttpGet]
         public Task<ActionResult<List<Device>>> GetAllDevices() {
             return Task.FromResult<ActionResult<List<Device>>>(_context.Devices.AsEnumerable()
-                .OrderBy(d => d.State != 1 ? d.State == 0 ? 1 : d.State == -1 ? 2 : 3 : 0).ThenBy(d => ip2uint(d.Ip))
+                .OrderBy(d => d.State != 1 ? d.State == 0 ? 1 : d.State == -1 ? 2 : 3 : 0).ThenBy(d => Ip2Uint(d.Ip))
                 .ThenBy(d => d.Mac).ToList());
         }
 
-        private long ip2uint(string? dIp) {
+        private static long Ip2Uint(string? dIp) {
             try {
                 var ip = dIp.Split(",")[0];
                 var octets = ip.Split(".");
                 return (int.Parse(octets[0]) * 1000 * 1000 * 1000) + (int.Parse(octets[1]) * 1000 * 1000) + (int.Parse(octets[2]) * 1000) + int.Parse(octets[3]);
             } catch (Exception e) {
+                Console.WriteLine(e.Message);
                 return long.MaxValue;
             }
         }
